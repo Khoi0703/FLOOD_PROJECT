8e574628a29093aae0bb194b586cad38b32354db
DengKhoi <nguyendangkhoi9517@gmail.com>
2025-06-21 16:21:08 +0700

    Add asset

--- jobs/model_job.py
+++ jobs/model_job.py
@@ -3,12 +3,14 @@ from dagster import define_asset_job
 model_job = define_asset_job(
     name="model_job",
     selection=[
+    # ⬅️ thêm dòng này
         "water_cluster",
         "data_loading",
         "preprocessing",
         "model",
         "training",
         "evaluation",
+        "yenbai_rain",  
         "predict_yenbai",
         "utils"
     ]
